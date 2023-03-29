626de61de47e06b3f6813a06873cadfaf476bf4d
Isaac Overstreet <isaacoverstreet3@gmail.com>
2023-03-28 22:03:22 -0700

    Add timer for 30s game time

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 
-$("#play").on("click", function(){
+$("#play").on("click", function () {
   $(".game-page").show();
   $(".home-page").hide();
 });
 
-$("#home").submit(function(){});
+$("#home").submit(function () {});
 
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
 let cardFlipped = false;
 let lockTransition = false;
@@ -23,7 +23,7 @@ function flipCard() {
   if (this === firstCard) {
     return false;
   }
-  this.classList.add('flip');
+  this.classList.add("flip");
 
   if (!cardFlipped) {
     cardFlipped = true;
@@ -43,8 +43,8 @@ function checkMatch() {
 }
 
 function disableCards() {
-  $(firstCard).off('click', flipCard);
-  $(secondCard).off('click', flipCard);
+  $(firstCard).off("click", flipCard);
+  $(secondCard).off("click", flipCard);
   reset();
 }
 
@@ -52,8 +52,8 @@ function unflipCards() {
   lockTransition = true;
 
   setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
     reset();
   }, 1500);
 }
@@ -64,10 +64,40 @@ function reset() {
 }
 
 (function random() {
-  cards.forEach(card => {
+  cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
 })();
 
-cards.forEach(card => $(card).on('click', flipCard));
+cards.forEach((card) => $(card).on("click", flipCard));
+
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (++timer < 0) {
+      timer = 0;
+      // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+    } else if (++timer >= 34) {
+      timer = 0;
+      $(".game-page").hide();
+      $(".home-page").show();
+    }
+  }, 1000);
+}
+
+$("#play").click(function() {
+  var time = 0, // your time in seconds here
+    display = document.querySelector("#safeTimerDisplay");
+  startTimer(time, display);
+});
